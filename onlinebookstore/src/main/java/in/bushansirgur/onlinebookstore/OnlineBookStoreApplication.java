2e36b518b502aa36fbec9b8ac42f730159af62ab
scbushan05 <sc.bushan.05@gmail.com>
2020-01-29 22:24:39 +0530

    Intial commit. created spring boot project and configure the application properties

---
+++ onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/OnlineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
+	}
+
+}
---
+++ onlinebookstore/src/test/java/in/bushansirgur/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package in.bushansirgur.onlinebookstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineBookStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
